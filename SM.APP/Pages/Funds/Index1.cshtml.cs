d98804a533503ae80123e043b4556675f840080b
Mina Kozman <mina.nagy87@gmail.com>
2025-06-30 11:21:13 +0300

    add Funds listing layout design v1

---
+++ SM.APP/Pages/Funds/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SM.APP.Pages.Funds
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
