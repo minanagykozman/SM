b35744a0f7becaa76df151cd940046b308ded1c0
Mina Kozman <mina.nagy87@gmail.com>
2026-01-01 18:31:27 +0200

    Add event stats

---
+++ SM.APP/Pages/Events/EventStatistics.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SM.APP.Services;
+
+namespace SM.APP.Pages.Events
+{
+    public class EventStatisticsModel(ILogger<EventAttendanceModel> logger) : PageModelBase(logger)
+    {
+       
+    }
+}
